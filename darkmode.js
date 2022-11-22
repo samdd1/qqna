09e63615efe5f5fada6c1b4e9c1c227e24904c36
samdd1 <109146974+samdd1@users.noreply.github.com>
2022-11-23 00:05:12 +0530

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,13 @@
+var body = document.getElementById("body");
+var button = document.getElementById("button");
+
+button.onclick = function(){
+    if(body.className == "black"){
+    body.className = "white";
+    button.className = "black";
+}
+else{
+    body.className = "black";
+    button.className = "white";
+}
+}
